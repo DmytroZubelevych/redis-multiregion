d74800de3c1fbfac640f9439fdfa087e1d33e896
DmytroZubelevych <31444413+DmytroZubelevych@users.noreply.github.com>
2022-09-12 16:23:24 +0300

    Update onBeforeInit.js

--- scripts/onBeforeInit.js
+++ scripts/onBeforeInit.js
@@ -7,7 +7,7 @@ var regions = jelastic.env.control.GetRegions(appid, session);
 if (regions.result != 0) return regions;
 
 if (regions.array.length < 3) {
-  fields["count"].markup = "Package cannot be installed on less than 3 regions. Please contact support or choose a <a href='https://jelastic.cloud/?regions=multiregion' target='_blank'>provider</a> with more regions";
+  markup = "Package cannot be installed on less than 3 regions. Please contact support or choose a <a href='https://jelastic.cloud/?regions=multiregion' target='_blank'>provider</a> with more regions";
   settings.fields.push(
     {"type": "displayfield", "cls": "warning", "height": 30, "hideLabel": true, "markup": markup},
     {"type": "compositefield","height": 0,"hideLabel": true,"width": 0,"items": [{"height": 0,"type": "string","required": true}]}
